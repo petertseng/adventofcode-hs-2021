c86bf054ba44e00ec34610a902978435c391acd4
Peter Tseng <pht24@cornell.edu>
2021-12-22 16:24:32 +0000

    day 22: Use Inclusion-exclusion
    It's only slightly faster, maybe 10%? Well, better than nothing I
    suppose.

--- bin/22_reactor_reboot.hs
+++ bin/22_reactor_reboot.hs
@@ -1,28 +1,25 @@
 import AdventOfCode (readInputFile)
 import AdventOfCode.Split (splitOn)
 
+import Data.Either (partitionEithers)
+import Data.List (foldl')
+import Data.Maybe (mapMaybe)
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 type Interval = (Int, Int)
 type Box = (Interval, Interval, Interval)
 data Command = On | Off deriving (Eq, Show)
 
--- according to the commands, how many cells are lit within the specified box?
--- first command that matches wins.
-countOn :: [(Command, Box)] -> Box -> Int
-countOn _ b | boxEmpty b = 0
-countOn [] _ = 0
-countOn ((cmd, b) : xs) b2 = let inter = boxInter b b2 in
-  if boxEmpty inter then countOn xs b2
-  else (if cmd == Off then 0 else boxSize inter) + sum (map (countOn xs) (b2 `boxSub` inter))
-
-boxSub :: Box -> Box -> [Box]
-((x1, x2), y@(y1, y2), z@(z1, z2)) `boxSub` (xi@(xi1, xi2), yi@(yi1, yi2), (zi1, zi2)) =
-  [ ((x1, xi1 - 1), y, z)
-  , ((xi2 + 1, x2), y, z)
-  , (xi, (y1, yi1 - 1), z)
-  , (xi, (yi2 + 1, y2), z)
-  , (xi, yi, (z1, zi1 - 1))
-  , (xi, yi, (zi2 + 1, z2))
-  ]
+-- inclusion-exclusion
+combineBox :: Map Box Int -> (Command, Box) -> Map Box Int
+combineBox boxes (onoff, b) = Map.filter (/= 0) updated
+  where (deletes, updates) = partitionEithers (mapMaybe check (Map.assocs boxes))
+        check (k, _) | b `boxSuper` k = Just (Left k)
+        check (k, v) = let inter = boxInter k b in if boxEmpty inter then Nothing else Just (Right (inter, -v))
+        deleted = foldl' (flip Map.delete) boxes deletes
+        updates' = if onoff == On then (b, 1) : updates else updates
+        updated = Map.unionWith (+) deleted (Map.fromListWith (+) updates')
 
 boxInter :: Box -> Box -> Box
 boxInter (x1, y1, z1) (x2, y2, z2) = (interInter x1 x2, interInter y1 y2, interInter z1 z2)
@@ -33,18 +30,21 @@ boxSize (x, y, z) = interSize x * interSize y * interSize z
 boxEmpty :: Box -> Bool
 boxEmpty (x, y, z) = interEmpty x || interEmpty y || interEmpty z
 
+boxSuper :: Box -> Box -> Bool
+(x1, y1, z1) `boxSuper` (x2, y2, z2) = x1 `interSuper` x2 && y1 `interSuper` y2 && z1 `interSuper` z2
+
 interInter :: Interval -> Interval -> Interval
 interInter (a, b) (c, d) = (max a c, min b d)
 
 interSize :: Interval -> Int
+interSize (a, b) | a > b = 0
 interSize (a, b) = b - a + 1
 
 interEmpty :: Interval -> Bool
 interEmpty (a, b) = a > b
 
-minmax :: [Interval] -> Interval
-minmax xs = (minimum ls, maximum rs)
-  where (ls, rs) = unzip xs
+interSuper :: Interval -> Interval -> Bool
+(a, b) `interSuper` (c, d) = a <= c && d <= b
 
 boxinst :: String -> (Command, Box)
 boxinst s = case words s of
@@ -64,10 +64,8 @@ main :: IO ()
 main = do
   s <- readInputFile
   let insts = map boxinst (lines s)
-      boxes = map snd insts
       r50 = (-50, 50)
-      rev = reverse insts
-  print (countOn rev (r50, r50, r50))
-
-  let (xs, ys, zs) = unzip3 boxes
-  print (countOn rev (minmax xs, minmax ys, minmax zs))
+      c50 = (r50, r50, r50)
+      boxPolarity = Map.assocs (foldl' combineBox Map.empty insts)
+  print (sum [boxSize (boxInter b c50) * p | (b, p) <- boxPolarity])
+  print (sum [boxSize b * p | (b, p) <- boxPolarity])
